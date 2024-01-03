6fe32e30940eaaea787a852aebb6802926355e0a
Giuseppina Morena <giuseppinamorena@libero.it>
2024-01-03 16:31:11 +0100

    Aggiunto microservizio craftstore

--- craftstore-service/src/main/java/com/unito/edu/craftstoreservice/repository/CraftstoreRepository.java
+++ craftstore-service/src/main/java/com/unito/edu/craftstoreservice/repository/CraftstoreRepository.java
@@ -1,13 +1,9 @@
 package com.unito.edu.craftstoreservice.repository;
 
 import com.unito.edu.craftstoreservice.model.Craftstore;
-import com.unito.edu.craftstoreservice.model.dto.CraftstoreDto;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
 
 @Repository
 public interface CraftstoreRepository extends JpaRepository<Craftstore,Integer>, JpaSpecificationExecutor<Craftstore> {
